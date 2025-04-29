c15e26033a03b3ca31eff967e16498b553ccab6c
Mark <153409985+Devspacetechnologies@users.noreply.github.com>
2025-04-29 16:07:18 +0300

    Update animemd.js

--- plugins/animemd.js
+++ plugins/animemd.js
@@ -23,7 +23,7 @@ try{
 let res = await axios.get('https://api.lolicon.app/setu/v2?num=1&r18=0&tag=lolicon')
 let wm = `😎 Random Garl image
 
-©💫 NEXUS-XMD💫`
+©💫 𝐌𝐀𝐑𝐊 𝐓𝐄𝐂𝐇💫`
 await conn.sendMessage(from, { image: { url: res.data.data[0].urls.original }, caption: wm}, { quoted: mek })
 } catch (e) {
 reply(cants)
@@ -46,7 +46,7 @@ try{
 let res = await axios.get('https://api.waifu.pics/sfw/waifu')
 let wm = `🩵 Random Waifu image
 
-©💫 NEXUS-𝐗𝐌𝐃 💫`
+©💫 𝐌𝐀𝐑𝐊 𝐓𝐄𝐂𝐇 💫`
 await conn.sendMessage(from, { image: { url: res.data.url }, caption: wm}, { quoted: mek })
 } catch (e) {
 reply(cants)
@@ -69,7 +69,7 @@ try{
 let res = await axios.get('https://api.waifu.pics/sfw/neko')
 let wm = `🩷 Random neko image
 
-©💫 Nexus-𝐗𝐌𝐃 💫`
+©💫 𝐌𝐀𝐑𝐊 𝐓𝐄𝐂𝐇 💫`
 await conn.sendMessage(from, { image: { url: res.data.url  }, caption: wm}, { quoted: mek })
 } catch (e) {
 reply(cants)
@@ -92,7 +92,7 @@ try{
 let res = await axios.get('https://api.waifu.pics/sfw/megumin')
 let wm = `🔥 Random megumin image
 
-©💫 NEXUS-𝐗𝐌𝐃 💫`
+©💫 𝐌𝐀𝐑𝐊 𝐓𝐄𝐂𝐇💫`
 await conn.sendMessage(from, { image: { url: res.data.url }, caption: wm}, { quoted: mek })
 } catch (e) {
 reply(cants)
@@ -115,7 +115,7 @@ try{
 let res = await axios.get('https://api.waifu.im/search/?included_tags=maid')
 let wm = `😎 Random maid image
 
-©💫 NEXUS-𝐗𝐌𝐃 💫`
+©💫 𝐌𝐀𝐑𝐊 𝐓𝐄𝐂𝐇💫`
 await conn.sendMessage(from, { image: { url: res.data.images[0].url  }, caption: wm}, { quoted: mek })
 } catch (e) {
 reply(cants)
@@ -138,7 +138,7 @@ try{
 let res = await axios.get('https://api.waifu.pics/sfw/awoo')
 let wm = `😎 Random awoo image
 
-©💫 NEXUS-𝐗𝐌𝐃 💫`
+©💫 𝐌𝐀𝐑𝐊 𝐓𝐄𝐂𝐇 💫`
 await conn.sendMessage(from, { image: { url: res.data.url }, caption: wm}, { quoted: mek })
 } catch (e) {
 reply(cants)
@@ -280,7 +280,7 @@ cmd({
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
 
-let dec = `>💫 NEXUS-𝐗𝐌𝐃 💫 ANIME IMGS*`
+let dec = `>💫 𝐌𝐀𝐑𝐊 𝐓𝐄𝐂𝐇 💫 𝐀𝐍𝐈𝐌𝐄 𝐈𝐌𝐆𝐒*`
 await conn.sendMessage(from,{image:{url: `https://telegra.ph/file/b26f27aa5daaada031b90.jpg`},caption:dec},{quoted:mek});
 await conn.sendMessage(from,{image:{url: `https://telegra.ph/file/51b44e4b086667361061b.jpg`},caption:dec},{quoted:mek});
 await conn.sendMessage(from,{image:{url: `https://telegra.ph/file/7d165d73f914985542537.jpg`},caption:dec},{quoted:mek});
