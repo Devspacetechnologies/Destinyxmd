943ffd220e4d29158d559d0bf6c3ab8d4a214e6d
Mark <153409985+Devspacetechnologies@users.noreply.github.com>
2025-04-29 16:47:53 +0300

    Update news.js

--- plugins/news.js
+++ plugins/news.js
@@ -24,7 +24,7 @@ async (conn, mek, m, { from, reply }) => {
 ⚠️ _${article.description}_
 🔗 _${article.url}_
 
-  © Pkdriller NEXUS-XMD NEWS
+  © 𝐌𝐀𝐑𝐊 𝐓𝐄𝐂𝐇 
             `;
 
             console.log('Article URL:', article.urlToImage); // Log image URL for debugging
